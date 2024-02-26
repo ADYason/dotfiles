dff689d14e4911ee42ac30b09914d0b2ccb00402
ADYason <ADYason@apsolutions.ru>
2024-02-26 17:35:41 +0300

    added actual vim config

--- nvim/lua/custom/chadrc.lua
+++
@@ -1,6 +0,0 @@
----@type ChadrcConfig
-local M = {}
-
-M.ui = { theme = 'rxyhn' }
-
-return M
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,196 @@
+local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+end
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["J"] = cmp.mapping.scroll_docs(-4),
+    ["K"] = cmp.mapping.scroll_docs(4),
+    -- ['<leader>c'] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+        -- elseif luasnip.expand_or_jumpable() then
+        --   luasnip.expand_or_jump()
+        -- elseif has_words_before() then
+        --   cmp.complete() -- i dont know what is it and why is it here but since it foes not need to eat i shall let it stay till i figure out why does it fuck all the config up
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+  }),
+})
+
+cmp.setup.filetype("gitcommit", {
+  sources = cmp.config.sources({
+    { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+  }),
+})
+
+cmp.setup.filetype("python", {
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+  }),
+})
+
+cmp.setup.filetype("helm", {
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+  }),
+})
+
+cmp.setup.filetype("go", {
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+  }),
+})
+cmp.setup.filetype("go.mod", {
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+  }),
+})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local lsp_flags = {
+  debounce_text_changes = 150,
+}
+
+lspconfig["ruff_lsp"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+})
+
+lspconfig["pyright"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+})
+
+lspconfig["tsserver"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+})
+
+lspconfig["gopls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  cmd = { "gopls", "serve" },
+  filetypes = { "go", "go.mod" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  flags = lsp_flags,
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+})
+
+-- a plugin to use LSP with function type sifnatures
+if false then
+  require("lsp_signature").setup({
+    debug = false, -- set to true to enable debug logging
+    log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
+    -- default is  ~/.cache/nvim/lsp_signature.log
+    verbose = false, -- show debug line number
+
+    bind = true, -- This is mandatory, otherwise border config won't get registered.
+    -- If you want to hook lspsaga or other signature handler, pls set to false
+    doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+    -- set to 0 if you DO NOT want any API comments be shown
+    -- This setting only take effect in insert mode, it does not affect signature help in normal
+    -- mode, 10 by default
+
+    max_height = 12, -- max height of signature floating_window
+    max_width = 80, -- max_width of signature floating_window
+    noice = false, -- set to true if you using noice to render markdown
+    wrap = false, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
+
+    floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+
+    floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+    -- will set to true when fully tested, set to false will use whichever side has more space
+    -- this setting will be helpful if you do not want the PUM and floating win overlap
+
+    floating_window_off_x = 1, -- adjust float windows x position.
+    -- can be either a number or function
+    floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
+    -- can be either number or function, see examples
+
+    close_timeout = 4000, -- close floating window after ms when laster parameter is entered
+    fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
+    hint_enable = true, -- virtual hint enable
+    hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+    hint_scheme = "String",
+    hint_inline = function()
+      return false
+    end, -- should the hint be inline(nvim 0.10 only)?  default false
+    hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+    handler_opts = {
+      border = "double", -- double, rounded, single, shadow, none, or a table of borders
+    },
+
+    always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+
+    auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+    extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+    zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+
+    padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
+
+    transparency = nil, -- disabled by default, allow floating win transparent value 1~100
+    shadow_blend = 36, -- if you using shadow as border use this set the opacity
+    shadow_guibg = "Black", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+    timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
+    toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+    toggle_key_flip_floatwin_setting = false, -- true: toggle float setting after toggle key pressed
+
+    select_signature_key = "<A-m>", -- cycle to next signature, e.g. '<M-n>' function overloading
+    move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
+  })
+end
+return {}
