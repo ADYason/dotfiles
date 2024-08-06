b403b4a3ded897fc940a25a48efd82d8d3662d6e
ADYason <ADYason@apsolutions.ru>
2024-08-06 18:08:18 +0300

    update nvim config

--- nvim/lua/dap-config.lua
+++ nvim/lua/dap-config.lua
@@ -28,39 +28,6 @@ end
 
 dap.repl.omnifunc = vim.lsp.omnifunc
 
-dap.adapters.cppdbg = {
-	id = "cppdbg",
-	type = "executable",
-	command = "/home/asu/miscellaneous/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7",
-}
-
-dap.configurations.cpp = {
-	{
-		name = "Launch file",
-		type = "cppdbg",
-		request = "launch",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-		cwd = "${workspaceFolder}",
-		stopAtEntry = true,
-	},
-	{
-		name = "Attach to gdbserver :1234",
-		type = "cppdbg",
-		request = "launch",
-		MIMode = "gdb",
-		miDebuggerServerAddress = "localhost:1234",
-		miDebuggerPath = "/usr/bin/gdb",
-		cwd = "${workspaceFolder}",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-	},
-}
-dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
-
 dap.adapters.python = function(cb, config)
 	if config.request == "attach" then
 		---@diagnostic disable-next-line: undefined-field
